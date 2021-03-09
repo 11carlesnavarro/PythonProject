d16a4daf98c72e52aff110e53cd8e8db79d4b078
RicardoFong <33761911+RicardoFong@users.noreply.github.com>
2021-03-09 14:38:50 +0100

    Update RicStarting.py

--- RicStarting.py
+++ RicStarting.py
@@ -5,13 +5,16 @@ import os
 import argparse
 import re
 
-parser = argparse.ArgumentParser(description="""This program reconstructs the macrocomplex from protein interactions pdb files""")
+parser = argparse.ArgumentParser(description = """This program analyze pdb binary chain interactions provided, calculate and reconstructs the polipeptide complex
+represented by the provided files. 
+""")
 
 parser.add_argument('-i', '--input',
                                         dest = "infile",
                                         action = "store",
-                                        default = os.getcwd(),
-                                        help = "Enter a list of interaction files, a directory or the current directory will be selected")
+                                        default = os.getcwd(), #current input default is complete current directory, we need to discuss what default will be set.
+                                        help = """You need to provide the path to the directory where the binary interaction pdb files are stored, 
+                                                  they will be all analized""") # I think we could use default = None, and guide the user to provide only a path. Discussion.
 
 parser.add_argument('-o', '--output',
                                         dest = "outfile",
@@ -24,10 +27,18 @@ args = parser.parse_args()
 ###########################################################################################################################
 
 #PROPIO#########################################################
-#if  __name__=="__main__":
+
+# En la investigación vi que exiten por lo menos tres instancias de interés para analizar los pdb:
+# Structure, Chain y sequence. Structure está representada por la totalidad del pdb file, chain se
+# refiere al nombre de la cadena, en los binary interaction files habrá solo dos cadenas. Sequence 
+# es como su nombre lo dice la sequencia. En este caso de aminoácidos.
+
+# Basados en el texto anterior, estas dos líneas siguientes analizan los archivos pdb y almacenan la 
+# información de la estructura en general en objetos de python.
 structure1 = PDBParser().get_structure('test', "chainA.pdb")
 structure2 = PDBParser().get_structure('test', "chainB.pdb")
 
+# obtener la secuencia. El texto en la función es propio.
 def get_sequence(structure):
     """returns the sequence of the structure instance passed
     through the method. It uses CaPPBuilder from Bio.pdb module
@@ -40,6 +51,12 @@ def get_sequence(structure):
 
     return(seq)
 
+# En ésta función se hace un alineamiento global. después se calcula el sequence simlarity (SS)
+# con la fórmula   SS = score/len(minSeq)   en donde score es el valor del score obtenido en la 
+# alineación pareada. OJO !!! - éste cálculo es erroneo, se está construyendo esta función, estoy
+# investigando la fórmula necesarioa para calcular el SS de forma correcta. Se tiene que penalizar
+# de forma diferente el cambio de aminoácidos según las propiedades fisicoquímicas de los camvbios de
+# los residuos
 def compare_seqs(seq1, seq2):
     """Returns the percentage sequence similarity between
     two given sequences. """
@@ -53,7 +70,9 @@ def compare_seqs(seq1, seq2):
 
 #TESTING#################################################
 ######################
-#
+## éstas son funciones en construcción, no funcionan aún, y están siendo exploradas. Las funciones de 
+## éstas vienen del paquete Bio.pdb
+
 # def get_name_structure(pdb_files):
 #         """Parses the names of the input files and returns the name of the files without .pdb extension"""
 #         p = re.compile('(.*).pdb') #using regular expression, impor re needed
@@ -89,6 +108,11 @@ def compare_seqs(seq1, seq2):
 
 if  __name__=="__main__":
 
+    # corriendo las dos funciones. ambas funcionan, se extraen las secuencias de las dos cadenas 
+    # (vienen de archivos independientes, hay que desarrollar la forma de hacerlo desde un solo archivo binario)
+    # tras extraer las secuencias, se hace el alineamiento pareado y se ejecuta la formula. Estoy trabajando 
+    # en obtener la fórmula correcta.
+    
     seq1 = get_sequence(structure1)
     seq2 = get_sequence(structure2)
 
