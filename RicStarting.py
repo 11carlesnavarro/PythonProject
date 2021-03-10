bdc796bcb922db7efa7c6635c5720745ebc4f687
RicardoFong <33761911+RicardoFong@users.noreply.github.com>
2021-03-11 00:14:17 +0100

    Update RicStarting.py

--- RicStarting.py
+++ RicStarting.py
@@ -1,6 +1,6 @@
 from Bio.PDB import *
 from Bio import pairwise2
-from Bio.SubsMat import MatrixInfo as matlist # Used for applying subtitution matrix in sequence comparison
+# from Bio.SubsMat import MatrixInfo as matlist # Used for applying subtitution matrix in sequence comparison
 from Bio.pairwise2 import format_alignment # DEBUGING Used for printing the alignment. DEBUGING purposes
 from math import log # For use in defining the gap function for sequence comparison parameters
 import sys
@@ -8,103 +8,85 @@ import os
 import argparse
 import re
 
-parser = argparse.ArgumentParser(description = """This program analyze pdb binary chain interactions provided, calculate and reconstructs the polipeptide complex
-represented by the provided files. 
+arser = argparse.ArgumentParser(description = """This program analyze pdb binary chain interactions provided, calculate and reconstructs the polipeptide complex
+represented by the provided files.
 """)
 
 parser.add_argument('-i', '--input',
                                         dest = "infile",
                                         action = "store",
                                         default = os.getcwd(), #current input default is complete current directory, we need to discuss what default will be set.
-                                        help = """You need to provide the path to the directory where the binary interaction pdb files are stored, 
+                                        help = """You need to provide the path to the directory where the binary interaction pdb files are stored,
                                                   they will be all analized""") # I think we could use default = None, and guide the user to provide only a path. Discussion.
 
 parser.add_argument('-o', '--output',
                                         dest = "outfile",
                                         action = "store",
                                         default = None,
-                                        help = "Enter the name of output file")
+                                        help = """The output file will be named after the name provided after this flag.
+                                                This option is mandatory, if no output filename is provided, no analysis
+                                                will be run and this message will be displayed. """)
+
+parser.add_argument('-v', '--verbose',
+                                        dest = "verbose",
+                                        action = "store_true",
+                                        default = False,
+                                        help = """This option prints the execution of the program as it is the actions are being.
+                                                performed. It is used without options as it activates the verbose mode on.
+                                                It is adviseable to run the analysis with this option in case any exception is raised.""")
 
 args = parser.parse_args()
 
 ###########################################################################################################################
 
-# En la investigación vi que exiten por lo menos tres instancias de interés para analizar los pdb:
-# Structure, Chain y sequence. Structure está representada por la totalidad del pdb file, chain se
-# refiere al nombre de la cadena, en los binary interaction files habrá solo dos cadenas. Sequence 
-# es como su nombre lo dice la sequencia. En este caso de aminoácidos.
-
-# Basados en el texto anterior, estas dos líneas siguientes analizan los archivos pdb y almacenan la 
-# información de la estructura en general en objetos de python.
+# Por ahora la ejecución del programa funciona bien, pero toma solo éstas dos secuencias. Debe cambiarse para operar sobre 
+# todos los archivos del directorio y desde los archivos pareados, analizándolos sin separar.
 structure1 = PDBParser().get_structure('test', "chainA.pdb")
-structure2 = PDBParser().get_structure('test', "chainB.pdb")
+structure2 = PDBParser().get_structure('test', "chainC.pdb")
 
-# obtener la secuencia. El texto en la función es propio.
+# regresa la secuencia de un archivo pdb. Debe trabajar sobre un solo archivo pareado.
 def get_sequence(structure):
     """returns the sequence of the structure instance passed
-    through the method. It uses CaPPBuilder from Bio.pdb module
-    so it identifies each alpha Carbon of the peptide chain to
-    build the sequence"""
+       through the method. It uses CaPPBuilder from Bio.pdb module
+       so it identifies each alpha Carbon of the peptide chain to
+       build the sequence"""
 
     ppb = CaPPBuilder()
     for pp in ppb.build_peptides(structure):
         seq = pp.get_sequence()
 
     return(seq)
-  
- def gap_opening_function(x, y): # x is gap position in seq, y is gap length
-    """ This function handles the penalties of gap openings and extensions in
-    compare_seqsBLOSUM method. change the values on the return funtions for changing
-    the penalty values. """
 
-    if y == 0: # No gap
-        return 0
-    elif y == 1: #Gap open penalty
-        return -2
-    return - (2 + y/4.0 + log(y)/2.0)
+# Funciona bien, debe recibir dos secuencias separadas. El cálculo de la identidad de secuencias es correcto.
+def compare_seqs(seq1, seq2):
+    """This method aligns two given sequences, calculates percentage identity and
+       tests if this identity is equal or higher than 95% or lower. If the given
+       sequences show 95% identity or more, method returns True. otherwise it returns
+       False."""
+    # value 1 is for counting identical matches. Therefore the alignment score is
+    # equal to counting identical matches. percentage identity is then calculated
+    # as the ratio between the score and the length of the alignment. this percentage
+    # identity is the BLAST definition.
 
-def gap_extension_function(x, y): # x is gap position in seq, y is gap length
-    """ This function handles the penalties of gap openings and extensions in
-    compare_seqsBLOSUM method. change the values on the return funtions for changing
-    the penalty values. """
+    # definition of BLAST percentage identity is taken from:
+    # Heng Li, 2021, at: https://lh3.github.io/2018/11/25/on-the-definition-of-sequence-identity
+    align = pairwise2.align.globalmx(seq1, seq2, 1, 0)
+    algn_len = len(align[0][1])
+    match_score = align[0][2]
 
-    if y == 0: # No gap
-        return 0
-    elif y == 1: #Gap open penalty
-        return -2
-    return - (2 + y/4.0 + log(y)/2.0)
+    identity = match_score/algn_len
 
-# En ésta función se hace un alineamiento global. después se calcula el sequence simlarity (SS)
-# con la fórmula   SS = score/len(maxSeq)   en donde score es el valor del score obtenido en la 
-# alineación pareada. se utiliza la matriz de substitución BLOSUM50, pero puede ser elegida alguna
-# otra. Los penalties de gap opening y gap extension pueden ser cambiados en las funciones de:
-# gap_opening_function y gap_extension_function
+    # Checking for identity, true = homodimer. false = heterodimer
+    if identity >= .95:
+        return True
+    else:
+        return False
 
-# OJO!!, aún falta confirmar si aunque tengamos matriz de substitución y penalties, es pertinente
-# dividir el score entre la secuencia de máxima longitud para obtener el porcentaje de similitud.
-def compare_seqsBLOSUM50(seq1, seq2):
-    """This method returns the score of a pairwise sequence comparison. Expects
-    two sequences. The matrix substitution is blosum50, and penalizes gap openings
-    and extensions with values set in gap_function. default penalties are :
 
-    gap opening: -2 # I am exploring which penalties are best for gap opening and extension.
-    gap extension: -2 """
+if __name__=="__main__":
 
-    matrix = matlist.blosum50
-    alignment = pairwise2.align.globaldc(seq1, seq2, matrix, gap_opening_function, gap_extension_function)
-    score = alignment[0][2]
-    seq_sim = score/max(len(seq1), len(seq2))
-
-    return seq_sim
-
-if  __name__=="__main__":
-
-    # corriendo las funciones. corren sin error, pero con un warning que estoy trabajando. se extraen las secuencias de las dos cadenas 
-    # (vienen de archivos independientes, hay que desarrollar la forma de hacerlo desde un solo archivo binario)
-    # tras extraer las secuencias, se hace el alineamiento pareado y se ejecuta la formula. Estoy trabajando 
-    # en obtener la fórmula correcta.
-    
     seq1 = get_sequence(structure1)
     seq2 = get_sequence(structure2)
 
-    compare_seqs(seq1, seq2)
+    compare = compare_seqs(seq1, seq2)
+    print(compare)
